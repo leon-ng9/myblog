ba32e0fa0f27968a814b6bbe4e047c9bc2239b88
leon-ng9 <leon.ng9@outlook.com>
2016-07-20 15:41:10 +1000

    Add monthly archive

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
+    url(r'^posts/(?P<year>\d+)/(?P<month>\d+)/$', views.post_month, name='post_month'),
     url(r'', views.post_list, name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,23 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from .forms import PostForm
 from .models import Post
+import calendar
 
 # Create your views here.
+
+def mkmonth_lst():
+    """Make a list of months to show archive links."""
+
+    post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+    months = []
+    for post in post_list:
+        y = post.published_date.year
+        m = post.published_date.month
+        if (m not in [month[1] for month in months]):
+            months.append((y, m, calendar.month_name[m]))
+
+    return months
 
 def post_list(request):
     post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
@@ -20,11 +35,11 @@ def post_list(request):
         # if page is out of range (e.g. 9999), deliver last page of results.
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts, 'months': mkmonth_lst()})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html', {'post': post, 'months': mkmonth_lst()})
 
 def post_new(request):
     if request.method == "POST":
@@ -69,3 +84,19 @@ def post_delete(request, pk):
             return redirect('post_detail', pk=post.pk)
 
     return render(request, 'blog/post_delete.html', {})
+
+def post_month(request, year, month):
+    post_list = Post.objects.filter(published_date__year=year, published_date__month=month).order_by('-published_date')
+    paginator = Paginator(post_list, 5) # number of posts per page
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post_month.html', {'posts': posts, 'months': mkmonth_lst()})
